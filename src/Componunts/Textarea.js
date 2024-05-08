b9ceef20c4087e7cf899b8ad905407cb50107fa0
Codrix1 <152642319+Codrix1@users.noreply.github.com>
2024-05-09 01:58:04 +0300

    Add files via upload

--- src/Componunts/Textarea.js
+++ src/Componunts/Textarea.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useRef, useState } from "react";
 
 const  Textarea = () => {
+    let word , arraysize , counterstate;
     const [text, setText] = useState('');
     const [Variable , setVariable] = useState("");
     const [Identifiers , setIdentifiers] = useState("");
     const [Reserved_Words , setReserved_Words] = useState(""); 
     const [Symbols , setSymbols] = useState("");
-
+    const [Numbers , setNumbers] = useState("");
+    const [Arrays , setArrays] = useState("");
     const textareaRef = useRef(null);
     const handleInputChange = (event) => {
         const { value } = event.target;
@@ -14,23 +16,32 @@ const  Textarea = () => {
     };
     const numRows = text.split('\n').length;
     
-    useEffect((Variable , Identifiers , Reserved_Words , Symbols ) => { 
-      const string = textareaRef.current.value.split(/\s+/); 
+    useEffect((Variable , Identifiers , Reserved_Words , Symbols , word , Numbers  , arraysize , counterstate) => {
       setIdentifiers("");
       setVariable("");
       setReserved_Words("");
       setSymbols("");
-      string.forEach(word => {
-        console.log(word);
-        switch(word){
+      setNumbers("");
+      setArrays("");
+      word="";
+      arraysize = 0;
+      counterstate = false;
+      const checkIdentifier = (word) =>
+        {
+          switch(word){
             case "int" :
             case "float" :
             case "double" :
             case "bool" :
             case "char" :
-            setIdentifiers((prevIdentifiers) => prevIdentifiers + word + " , ");
-              break;
-
+              return true;
+            default:
+              return false;
+          }
+        }
+      const checkReservedWords = (word) =>
+        {
+          switch(word){
             case "if":
             case "do":
             case "for":
@@ -38,75 +49,104 @@ const  Textarea = () => {
             case "return":
             case "break":
             case "continue":
-            case "end":
-              setReserved_Words((prevReserved_Words) => prevReserved_Words + word + " , ");
-              break;
-
-            case "/" :
-            case "+" :  
-            case "-" :
-            case "*" :  
-              setSymbols((prevSymbols) => prevSymbols +"operator , ");
-              break;
-            
-            case "(" :  
-              setSymbols((prevSymbols) => prevSymbols +"Open Bracket , ");
-              break;
-            case ")" :  
-              setSymbols((prevSymbols) => prevSymbols +"Close Bracket , ");
-              break;
-            case "{" :  
-              setSymbols((prevSymbols) => prevSymbols +"Open Curly Bracket , ");
-              break;
-            case "}" :  
-              setSymbols((prevSymbols) => prevSymbols +"close Curly Bracket , ");
-              break;
-            case "," :  
-              setSymbols((prevSymbols) => prevSymbols +"Comma , ");
-              break;
-            case ";" :  
-              setSymbols((prevSymbols) => prevSymbols +"Semicolon , ");
-              break;
-            case "&&" :  
-              setSymbols((prevSymbols) => prevSymbols +"And , ");
-              break;
-            case "||" :  
-              setSymbols((prevSymbols) => prevSymbols +"Or , ");
-              break;
-            case "<" :  
-              setSymbols((prevSymbols) => prevSymbols +"Less than , ");
-              break;
-            case ">" :  
-              setSymbols((prevSymbols) => prevSymbols +"Greater Than , ");
-              break;
-            case "=" :  
-              setSymbols((prevSymbols) => prevSymbols +"Equal , ");
-              break;
-            case "!" :  
-              setSymbols((prevSymbols) => prevSymbols +"Not , ");
-              break;
-
+            case "end ":
+              return true;
             default:
-              setVariable((prevVariable) => prevVariable + word + " , ");
-              break;
-            }});
-      if(Variable!=="")
+              return false;
+            }
+        }
+      const check =(word)=>
         {
+          if(checkIdentifier(word)){
+            setIdentifiers((prevIdentifiers) => prevIdentifiers + word + " , ");
+          }
+          else if (checkReservedWords(word)){
+            setReserved_Words((prevReserved_Words) => prevReserved_Words + word + " , ");
+          }
+          else if (isNaN(word)) {
+            setVariable((prevVariable) => prevVariable + word + " , ");
+          }
+          else{
+            setNumbers((prevNumber) => prevNumber + word + " , ");
+          }
+        }
+      const addarray = (word , arraysize) =>{
+        setArrays((prevArray)=>prevArray + word + " [" + arraysize.toString() + "] , " );
+      }  
+ 
+      const letters = textareaRef.current.value.split('')      
+      letters.forEach((letter) => {
+        switch(letter){ 
+          case "/" :
+          case "+" :  
+          case "-" :
+          case "*" :
+          case "(" : 
+          case ")" :
+          case "{" :
+          case "}" :
+          case ";" :
+          case "&&" :
+          case "||" :
+          case "<" :  
+          case ">" :  
+          case "=" :
+          case "!" :  
+            setSymbols((prevSymbols) => prevSymbols +letter+" , ");
+            if(word !== "" && !counterstate){
+              check(word);
+              word=""; }
+              break
+          case " ":
+          case '\n':
+          case '\t':
+            if(word !== "" && !counterstate){
+              check(word);
+              word=""; }
+            break;
+          case "[":
+            if( !checkIdentifier(word) && !checkReservedWords(word) && (isNaN(word) || word=="")){
+                arraysize =1;
+                counterstate = true;
+            }
+            break;
+          case "]":
+            if(counterstate){
+                addarray(word , arraysize);
+                counterstate = false   
+                word="";
+              }
+            break
+          case ",":
+            if(counterstate){
+              arraysize +=1;
+            }
+            break;    
+          default:
+            if(!counterstate){              
+              word = word + letter;
+            }
+            break;
+          }
+        }
+      );
+      console.log(counterstate);
+      console.log(arraysize);
+      if(Variable!==""){
           setVariable((prevVariable) => prevVariable.substring(0, prevVariable.length - 3) );
         }
-      if(Symbols!=="")
-        {
+      if(Symbols!==""){
           setSymbols((prevSymbols) => prevSymbols.substring(0, prevSymbols.length - 3) );
         }
-      if(Reserved_Words!=="")
-        {
+      if(Reserved_Words!==""){
           setReserved_Words((prevReserved_Words) => prevReserved_Words.substring(0, prevReserved_Words.length - 3) );
         }
-      if(Identifiers!=="")
-        {
+      if(Identifiers!==""){
           setIdentifiers((prevIdentifiers) => prevIdentifiers.substring(0, prevIdentifiers.length - 3) );
-        }  
-                 
+        }
+      if(Numbers!==""){
+          setNumbers((prevNumbers) => prevNumbers.substring(0, prevNumbers.length - 3) );
+        }     
       },
         [text])
 
@@ -131,6 +171,8 @@ const  Textarea = () => {
         {Symbols && <li class="table-row"> <div class="col col-1">Symbols</div> <div class="col col-2">{Symbols}</div> </li>}
         {Reserved_Words && <li class="table-row"> <div class="col col-1">Reserved_Words</div> <div class="col col-2">{Reserved_Words}</div> </li>}
         {Variable && <li class="table-row"> <div class="col col-1">Variable</div> <div class="col col-2">{Variable}</div> </li>}
+        {Numbers && <li class="table-row"> <div class="col col-1">Numbers</div> <div class="col col-2">{Numbers}</div> </li>}
+        {Arrays && <li class="table-row"> <div class="col col-1">Arrays</div> <div class="col col-2">{Arrays}</div> </li>}
 
       </ul>
     
